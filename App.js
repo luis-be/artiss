db6d24bb4a43ba138c94dc446ea4a75aa2417d3a
[Luis Benitez] <[luis.benitez.ayala@gmail.com]>
2023-04-18 16:17:48 -0400

    Avanzado y colocado un boton

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-#red text-bg font-bold mt-2">Mariela te amo mucho</Text>
+      <Button title="Click me"></Button>
+      
     </View>
-  );
+  ); 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
