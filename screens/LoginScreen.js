b264455329e2af5eed95bad9c161b334067a153b
[Luis Benitez] <[luis.benitez.ayala@gmail.com]>
2023-04-19 15:32:12 -0400

    En medio de la autenticación.

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import useAuth from '../hooks/useAuth'
 
 const LoginScreen = () => {
+    const {user} = useAuth();
+    console.log(user);
   return (
     <View>
       <Text>loguearte a la app</Text>
